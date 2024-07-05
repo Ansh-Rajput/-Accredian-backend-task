9f1a128e7d3ada46272b0c5b09bcce9f6cb3913b
Ansh-Rajput <anshravikrish425317@gmail.com>
2024-07-05 21:32:34 +0530

    updated cors policy

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://accredian-frontend-task-alpha-opal.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
